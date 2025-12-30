8caa7c00749944d11baba18f4e69d53f581b1a3c
Ojas Manchanda <ojasmanchanda27@gmail.com>
2025-12-30 15:37:30 +0530

    mongo errors

--- init/index.js
+++ init/index.js
@@ -1,26 +1,42 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
-
 const mongoose = require("mongoose");
-const initData = require("./data.js");
+const rawData = require("./data.js");
 const Ticket = require("../models/ticket.js");
 
 const MONGO_URL = process.env.ATLASDB_URL;
 
+async function seedDB() {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("Connected to Atlas");
 
-async function main() {
-  await mongoose.connect(MONGO_URL);
-  console.log("Connected to Atlas");
+    const fixedData = rawData.map(ticket => ({
+      eventName: ticket.eventName,
+      eventDate: ticket.eventDate,
+
+      pricePerTicket: ticket.pricePerTicket,
+      ticketsAvailable: ticket.ticketsAvailable ?? ticket.totalSeats,
+
+      basePrice: ticket.basePrice ?? ticket.pricePerTicket,
+      totalSeats: ticket.totalSeats,
+
+      owner: ticket.owner,
+      location: ticket.location,
+      eventCategory: ticket.eventCategory,
+      description: ticket.description,
+    }));
+
+    await Ticket.deleteMany({});
+    const inserted = await Ticket.insertMany(fixedData);
+
+    console.log(`Inserted ${inserted.length} tickets`);
+    await mongoose.connection.close();
+  } catch (err) {
+    console.error("SEED ERROR:", err);
+    process.exit(1);
+  }
 }
 
-const initDB = async () => {
-  await Ticket.deleteMany({});
-  await Ticket.insertMany(initData);
-  console.log("Data successfully inserted");
-};
-
-main()
-  .then(initDB)
-  .then(() => mongoose.connection.close())
-  .catch((err) => console.error(err));
+seedDB();
